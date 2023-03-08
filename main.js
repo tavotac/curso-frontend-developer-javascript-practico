6ecaaca46d841a9da14894c39c9c41a437fb17c9
Gustavo Tacha <tavotac@gmail.com>
2023-03-07 22:29:41 -0500

    Segunda fusión, menu hamburguesa barra principal y ajustes de visualización en el diseño adaptable barras desplegables

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu')
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobilMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactivo');
+}
+
+function toggleMobilMenu(){
+    mobileMenu.classList.toggle('inactivo');
 }
