24a9ee57747b986d3fab56d0c78bc386ade366f9
Gustavo Tacha <tavotac@gmail.com>
2023-03-08 21:01:57 -0500

    Hoy se trabajo en la visualizacion de barras desplegables, se sobreponian

--- main.js
+++ main.js
@@ -1,16 +1,45 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobilMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactivo');
+
+    if(!isAsideClosed){
+         aside.classList.add('inactivo');
+    }
+
     desktopMenu.classList.toggle('inactivo');
 }
 
 function toggleMobilMenu(){
+   const isAsideClosed = aside.classList.contains('inactivo');
+
+   if(!isAsideClosed){
+        aside.classList.add('inactivo');
+   }
+
     mobileMenu.classList.toggle('inactivo');
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactivo');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactivo');
+    
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactivo')
+    }
+
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactivo')
+    }
+    
+    aside.classList.toggle('inactivo');
 }
